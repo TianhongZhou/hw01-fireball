0cc06244223f0c061a22ccd6cb52b43cc52d8cf6
Tianhong Zhou <teozhou1215@gmail.com>
2025-09-16 02:22:33 -0400

    Try publish

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {vec3, vec4, vec2} from 'gl-matrix';
-const Stats = require('stats-js');
+const Stats = require('stats.js');
 import * as DAT from 'dat.gui';
 import Icosphere from './geometry/Icosphere';
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
